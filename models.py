6446e1dfc1036dd86f908d7436d201ddfd4be76b
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-20 19:29:08 +0300

    refactored

--- models.py
+++ models.py
@@ -6,13 +6,16 @@ class User(models.Model):
     is_superuser = models.BooleanField(default=False)
 
 
-class Reciepe(models.Model):
+class Recipe(models.Model):
     ingredients = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
     picture = models.field_name = models.ImageField(upload_to=None)
     description = models.TextField()
+    ingredients = models. #Поле для списка всех Ingredients
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     amount = models.FloatField()
     measure = models.CharField(max_length=10)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+
