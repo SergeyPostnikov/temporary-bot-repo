1269ee05ef549e9ed2782a900e097025b37cd95c
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-20 19:00:10 +0300

    initial_commit

---
+++ models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    is_superuser = models.BooleanField(default=False)
+
+
+class Reciepe(models.Model):
+    ingredients = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    picture = models.field_name = models.ImageField(upload_to=None)
+    description = models.TextField()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.FloatField()
+    
