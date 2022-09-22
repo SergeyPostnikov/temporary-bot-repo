1b71099be7dab3c826510ffba7171916b4433ca4
Irina Kasatkina <kasatkina235@gmail.com>
2022-09-22 11:24:33 +0300

    handle user pressing keyboard button in Telegram bot

--- FoodPlan/recipe_bot/management/commands/recipe_bot.py
+++ FoodPlan/recipe_bot/management/commands/recipe_bot.py
@@ -1,9 +1,15 @@
+import collections
 import logging
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+# from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
 
 from .dialog_bot import DialogBot
 
@@ -20,12 +26,20 @@ class Command(BaseCommand):
                             filename='recipe_bot.log',
                             filemode='w'
         )
+        self.updater = Updater(token=settings.TELEGRAM_BOT_TOKEN)
+        bot_messages_handler = MessageHandler(Filters.text | Filters.command, self.bot_messages_handler)
+        self.updater.dispatcher.add_handler(bot_messages_handler)
+        bot_buttons_handler = CallbackQueryHandler(self.bot_buttons_handler)
+        self.updater.dispatcher.add_handler(bot_buttons_handler)
 
     def handle(self, *args, **kwargs):
-        dialog_bot = DialogBot(settings.TELEGRAM_BOT_TOKEN, self.bot_generator)
-        dialog_bot.start()
+        self.updater.start_polling()
+        self.updater.idle()
 
-    def bot_generator(self):
+    def bot_messages_handler(self, update, context):
+        if update.channel_post.text != "/start":
+            return
+
         question = ('Рад встрече!\n'
                     'Я могу подбирать для вас рецепты блюд.\n'
                     'Заинтересовались?\n'
@@ -34,9 +48,8 @@ class Command(BaseCommand):
                     'Для этого нам будут нужны ваше имя и телефон.\n'
                     'Хотите прочитать наше Соглашение об обработке персональных данных?'
         )
-        likes_python = yield from self.ask_yes_or_no(question)
-
-    def ask_yes_or_no(self, question):
+        # context.bot.send_message(chat_id=update.effective_chat.id, 
+        #                          text=question)        
         keyboard = [
              [
                  InlineKeyboardButton('Да', callback_data='да'),
@@ -44,7 +57,37 @@ class Command(BaseCommand):
              ],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        answer = yield question, reply_markup
-        # while not ("да" in answer.text.lower() or "нет" in answer.text.lower()):
-        #     answer = yield (question, reply_markup)
-        # return "да" in answer.text.lower()
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                 text=question, reply_markup=reply_markup)
+
+    def bot_buttons_handler(self, update, _):
+        query = update.callback_query
+        variant = query.data
+        print(variant)
+
+        # `CallbackQueries` требует ответа, даже если 
+        # уведомление для пользователя не требуется, в противном
+        #  случае у некоторых клиентов могут возникнуть проблемы. 
+        # смотри https://core.telegram.org/bots/api#callbackquery.
+        query.answer()
+        # редактируем сообщение, тем самым кнопки 
+        # в чате заменятся на этот ответ.
+        # query.edit_message_text(text=f"Выбранный вариант: {variant}")
+
+        # likes_python = yield from self.ask_yes_or_no(question)
+        # print(likes_python)
+        # if likes_python:
+        #     answer = yield from discuss_good_python(name)
+        # else:
+        #     answer = yield from discuss_bad_python(name)
+
+    # def ask_yes_or_no(self, question):
+    #     keyboard = [
+    #          [
+    #              InlineKeyboardButton('Да', callback_data='да'),
+    #              InlineKeyboardButton('Нет', callback_data='нет'),
+    #          ],
+    #     ]
+    #     reply_markup = InlineKeyboardMarkup(keyboard)
+    #     answer = yield question, reply_markup
+    #     return answer
