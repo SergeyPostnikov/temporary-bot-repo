8cf1c11bfb4c7251c35171c9c0695fcab7d09dac
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-24 19:39:43 +0300

    passenger changed

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-sys.path.insert(0, '/home/a/alekspbv/alekspbv.beget.tech/FoodPlan')  # python dir
+sys.path.insert(0, '/home/a/alekspbv/alekspbv.beget.tech/temporary-bot-repo/FoodPlan')  # python dir
 sys.path.insert(1, '/home/a/alekspbv/.local/bin/python3.8/site-packages')  # django dir
 os.environ['DJANGO_SETTINGS_MODULE'] = 'FoodPlan.settings'
 
