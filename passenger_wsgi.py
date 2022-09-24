41a833e04688ce91ca5f2a54ad22201b6469ab11
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-24 18:57:51 +0300

    deployment files added

---
+++ passenger_wsgi.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, '/home/a/alekspbv/alekspbv.beget.tech/FoodPlan')  # python dir
+sys.path.insert(1, '/home/a/alekspbv/.local/bin/python3.8/site-packages')  # django dir
+os.environ['DJANGO_SETTINGS_MODULE'] = 'FoodPlan.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
