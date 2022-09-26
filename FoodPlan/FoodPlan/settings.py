f0f6eb482f63c8ea096f4766a40721c7a7b64027
Sergey Postnikov <ser.post2017@gmail.com>
2022-09-26 15:55:40 +0300

    settings updated

--- FoodPlan/FoodPlan/settings.py
+++ FoodPlan/FoodPlan/settings.py
@@ -98,7 +98,8 @@ else:
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
             'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4' 
             },
         }
 
